feae3b572bcebb5223b66cc8b45dab7e83f15231
Winnerty <timofey.n.karpeev@gmail.com>
2025-03-14 17:34:42 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const test = require("./test")
+
+// MIDDLEWARE
+app.use((req, res, next) => {
+    test()
+    const val = 11 * 12 - 13
+    req.val = val
+    next()
+})
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+    res.send(req.val.toString())
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
