6a351ae862b10b3711ec4b62119c04ba108ef326
Winnerty <timofey.n.karpeev@gmail.com>
2025-03-17 15:06:15 +0100

    upd

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ app.use(express.json())
 
 app.use("/api/users", userRoutes)
 
+// cors middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+   "Access-Control-Allow-Headers",
+   "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  )
+  next()
+ })
+
 app.get("/", (req, res) => {
   res.send("Welcome to my API ! e-commerce backed 🤳")
  });
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const router = express.Router()
+const { hashPassword } = require("../middleware/passencrypt")
 
 router.get("/", (req, res) => {
-    res.send('User page')
+    res.send(req)
 })
 
-router.post("/", (req, res) => {
+router.post("/", hashPassword, (req, res) => {
     //console.log(req.body)
     //res.send("you've reached the post request")
-    const { firstName, email, password } = req.body
-    res.json( {firstName, email, password} )
+    const { firstName, email } = req.body
+    const hashedPassword = req.hashedPassword
+    res.json( {firstName, email, hashedPassword, _id: "test_id_123"} )
 })
 
 module.exports = router
